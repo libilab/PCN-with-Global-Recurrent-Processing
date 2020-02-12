55925a4b5c197ecb01523ae82fad5414956426ef
KHan <han424@purdue.edu>
2020-02-12 10:13:00 -0500

    modify the typo on the comment

--- run.py
+++ run.py
@@ -8,7 +8,7 @@ parser = argparse.ArgumentParser(description='PyTorch CIFAR100 Training')
 parser.add_argument('--cls', default=6, type=int, help='number of cycles')
 parser.add_argument('--model', default='PredNet', help= 'models to train')
 parser.add_argument('--gpunum', default=2, type=int, help='number of gpu used to train the model')
-parser.add_argument('--lr', default=0.01, type=float, help='number of gpu used to train the model')
+parser.add_argument('--lr', default=0.01, type=float, help='learning rate')
 args = parser.parse_args()
 
 train_prednet(model=args.model, cls=cls, gpunum=args.gpunum, lr=args.lr)
